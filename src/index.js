aa0b4dbbd1118fc56c2a3ae5a998aba21a8dcd2a
james lawrence <jamdlaw@yahoo.com>
2019-01-26 12:14:32 -0800

    imported bootstrap css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
