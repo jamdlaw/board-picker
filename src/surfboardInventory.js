3191b7310beb01c8d9274bb9de61aac6d718fdb9
james lawrence <jamdlaw@yahoo.com>
2019-02-01 17:49:22 -0800

    added new images and created the beginning of surfboard inventory

---
+++ src/surfboardInventory.js
@@ -0,0 +1,20 @@
+const surfboardInventory = {
+	board1: {
+		 product_id: 293391,
+		 type: "shortboard",	
+		 image: "/img/fourth-surfboards-fourth-doofer-ese-construction-fcsii-5-fin-surfboard-white-black.jpg",
+		 price:54599,
+		 status: "available"
+	},
+	board2: {
+		product_id: 319863,
+		type: "longboard",	
+		image:"img/modern-surfboard-modern-retro-single-fin-surfboard-ice-tint.jpg",
+		price:70244,
+		status: "available"
+	}
+
+};
+
+
+export default surfboardInventory;
